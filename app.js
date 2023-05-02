65b46cb22f0175e3f242d7bf2fd902b49de7a87d
unknown <szymon.dziewajs@gmail.com>
2023-05-02 12:56:32 +0200

    added div generat

--- app.js
+++ app.js
@@ -1,75 +1,132 @@
-const API_KEY = `604676856a874813ab8132618230105`;
-const modalContainer = document.querySelector("#modal__container");
-const btnAddCities = document.querySelector(".nav__container__button--style");
-const btnCloseModal = document.querySelector(".modal__container--close");
-const magnifying = document.querySelector(".modal__container--magnifying");
-const currentImg = document.querySelector(".currentImg");
-const cityName = document.querySelector(".cityName");
-const currentTemp = document.querySelector(".temp");
-const currentPressure = document.querySelector(".currentPressure");
-const currentHumidity = document.querySelector(".currentHumidity");
-const currentWind = document.querySelector(".currentWind");
-
+const API_KEY = `604676856a874813ab8132618230105`
+const modalContainer = document.querySelector('#modal__container')
+const btnAddCities = document.querySelector('.nav__container__button--style')
+const btnCloseModal = document.querySelector('.modal__container--close')
+const magnifying = document.querySelector('.modal__container--magnifying')
+const currentImg = document.querySelector('.currentImg')
+const cityName = document.querySelector('.cityName')
+const currentTemp = document.querySelector('.temp')
+const currentPressure = document.querySelector('.currentPressure')
+const currentHumidity = document.querySelector('.currentHumidity')
+const currentWind = document.querySelector('.currentWind')
 const containerBottom = document.querySelector('.cities__container__bottom')
 const ul = document.querySelector('.ul')
-const nextDay = document.querySelector(".day");
-const weather = document.querySelector(".weather");
-const tempDays = document.querySelector(".tempDays");
-const icons = document.querySelector(".icons");
+const nextDay = document.querySelector('.day')
+const weather = document.querySelector('.weather')
+const tempDays = document.querySelector('.tempDays')
+const icons = document.querySelector('.icons')
 
 const modalOpen = () => {
-  modalContainer.classList.remove("visible");
-};
+	modalContainer.classList.remove('visible')
+}
 const modalClose = () => {
-  modalContainer.classList.add("visible");
-};
+	modalContainer.classList.add('visible')
+}
+
+const cityDivGenerator = () => {
+	const cityDiv = document.querySelector('.section__city')
+
+	const newDiv = document.createElement('div')
+	const newBtn = document.createElement('button')
+	const circleExit = document.createElement('i')
+	const newDivTop = document.createElement('div')
+	const img = document.createElement('img')
+	const divInfo = document.createElement('div')
+	const newParagraph = document.createElement('p')
+	const newSpan = document.createElement('span')
+	const divSpecifications = document.createElement('div')
+	const ul = document.createElement('ul')
+	const liPressure = document.createElement('li')
+	const liHumidity = document.createElement('li')
+	const liWind = document.createElement('li')
+	const divBotton = document.createElement('div')
+	const newUl = document.createElement('ul')
+	const liDay = document.createElement('li')
+	const liWeather = document.createElement('li')
+	const imgIcon = document.createElement('img')
+	const liTempDays = document.createElement('li')
+
+	newDiv.setAttribute('class', 'cities__container')
+	newBtn.setAttribute('class', 'cities__container--close')
+	circleExit.setAttribute('class', 'fa-solid fa-circle-xmark')
+	newDivTop.setAttribute('class', 'cities__container__top')
+	img.setAttribute('class', 'currentImg')
+	divInfo.setAttribute('class', 'cities__container__top--info')
+	newParagraph.setAttribute('class', 'cityName')
+	newSpan.setAttribute('class', 'temp')
+	divSpecifications.setAttribute('class', 'cities__container__top--specification')
+	liPressure.setAttribute('class', 'currentPressure')
+	liHumidity.setAttribute('class', 'currentHumidity')
+	liWind.setAttribute('class', 'currentWind')
+	divBotton.setAttribute('class', 'cities__container__bottom')
+	newUl.setAttribute('class', 'ul')
+  liDay.setAttribute('class', 'day')
+  liWeather.setAttribute('class', 'weather')
+  imgIcon.setAttribute('class', 'icons')
+  liTempDays.setAttribute('class', 'tempDays')
+
+	cityDiv.appendChild(newDiv)
+	newDiv.append(newBtn)
+	newBtn.append(circleExit)
+	newDiv.append(newDivTop)
+	newDivTop.append(img)
+	newDivTop.append(divInfo)
+	divInfo.append(newParagraph)
+	divInfo.append(newSpan)
+	newDivTop.appendChild(divSpecifications)
+	divSpecifications.append(ul)
+	ul.appendChild(liPressure)
+	ul.appendChild(liHumidity)
+	ul.appendChild(liWind)
+	newDiv.appendChild(divBotton)
+	divBotton.appendChild(newUl)
+  newUl.append(liDay)
+  newUl.append(liWeather)
+  liWeather.append(imgIcon)
+  newUl.append(liTempDays)
+}
 
 const weatherApp = async () => {
-  let cityValue = document.querySelector("#city").value;
-  try {
-    const result =
-      await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityValue}&days=5
-`);
-    const data = await result.json();
+	let cityValue = document.querySelector('#city').value
+	try {
+		const result = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityValue}&days=5
+`)
+		const data = await result.json()
 
-    const weatherImg = data.current.condition.icon;
-    const city = data.location.name;
-    const temp = data.current.temp_c;
-    const pressure = data.current.pressure_mb;
-    const humidity = data.current.humidity;
-    const wind = data.current.vis_km;
-    const wind_dir = data.current.wind_dir;
-    // data.forecast.forEach((day,tempDays,weatherDays) => {
-    const day = data.forecast.forecastday;
-    // day.forEach(data => {
-    //   console.log(day);
-    console.log(day);
+		const weatherImg = data.current.condition.icon
+		const city = data.location.name
+		const temp = data.current.temp_c
+		const pressure = data.current.pressure_mb
+		const humidity = data.current.humidity
+		const wind = data.current.vis_km
+		const wind_dir = data.current.wind_dir
+		const day = data.forecast.forecastday
 
-    // });
-    day.forEach((data) => {
-      const days = data.date;
-      const weathers = data.day.condition.icon;
-      const temps = data.day.avgtemp_c;
-      const newUl = document.createElement('ul')
-      const newLi = document.createElement('li')
-      ul.appendChild(newUl)
-      containerBottom.appendChild(newLi)
-      nextDay.innerHTML = days;
-      icons.setAttribute("src", weathers);
-      tempDays.innerHTML = temps;
-    });
+		day.forEach(data => {
+			const days = data.date
+			const weathers = data.day.condition.icon
+			const temps = data.day.avgtemp_c
+			const newUl = document.createElement('ul')
+			const newLi = document.createElement('li')
+			ul.appendChild(newUl)
+			containerBottom.appendChild(newLi)
+			nextDay.innerHTML = days
+			icons.setAttribute('src', weathers)
+			tempDays.innerHTML = temps
+		})
 
-    currentImg.setAttribute("src", weatherImg);
-    cityName.innerHTML = city;
-    currentTemp.innerHTML = `${temp} °C`;
-    currentPressure.innerHTML = `${pressure} hPa`;
-    currentHumidity.innerHTML = `${humidity} %`;
-    currentWind.innerHTML = `${wind_dir} ${wind} m/s`;
-  } catch (err) {
-    console.log(err);
-  }
-};
-magnifying.addEventListener("click", weatherApp);
+		currentImg.setAttribute('src', weatherImg)
+		cityName.innerHTML = city
+		currentTemp.innerHTML = `${temp} °C`
+		currentPressure.innerHTML = `${pressure} hPa`
+		currentHumidity.innerHTML = `${humidity} %`
+		currentWind.innerHTML = `${wind_dir} ${wind} m/s`
+	} catch (err) {
+		console.log(err)
+	}
+}
 
-btnAddCities.addEventListener("click", modalOpen);
-btnCloseModal.addEventListener("click", modalClose);
+magnifying.addEventListener('click', cityDivGenerator, weatherApp)
+
+btnAddCities.addEventListener('click', modalOpen)
+btnCloseModal.addEventListener('click', modalClose)
